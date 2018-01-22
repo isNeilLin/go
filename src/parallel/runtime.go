afa94ec4ca9117167efeffb9b4a3288ae7d0d9f0
Lin Pengfei <1264766856@qq.com>
2018-01-22 12:03:00 +0800

    themes

--- src/parallel/runtime.go
+++ src/parallel/runtime.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"time"
 )
 
-func say(s string)  {
-	for i:=0; i<5; i++ {
-		runtime.Gosched()  //表示让CPU把时间片让给别人，下次某个时候继续恢复执行该goroutine
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(1*time.Millisecond) //表示让CPU把时间片让给别人，下次某个时候继续恢复执行该goroutine
+		fmt.Println(s)
+	}
+}
+
+func speak(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(1*time.Millisecond)
 		fmt.Println(s)
 	}
 }
 
 func main() {
 	go say("Golang") //开一个新的Goroutine执行
-	say("hello") 	//当前Goroutine执行
-	var input string
-	fmt.Scanln(&input) //
+	go speak("World")
+	say("hello") //当前Goroutine执行
 }
